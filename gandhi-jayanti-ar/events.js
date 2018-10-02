1212161a0874fda7e1135baab30b19ba0bfe4fba
Imaad Mohamed Khan <imaadmkhan1@gmail.com>
2018-10-02 19:15:43 +0530

    Update events.js

--- gandhi-jayanti-ar/events.js
+++ gandhi-jayanti-ar/events.js
@@ -1,12 +1,15 @@
 AFRAME.registerComponent('markerhandler', {
-    tick: function () {
-	   const animatedMarker = document.querySelector("#animated-marker");
-           var entity = document.querySelector('[sound]');
-         if (animatedMarker.object3D.visible == true) {
-            entity.components.sound.playSound();
-        } else {
-            entity.components.sound.pauseSound();
-        }
 
-     }
-});
+    init: function() {
+        const animatedMarker = document.querySelector("#animated-marker");
+
+        // every click, we make our model grow in size
+        animatedMarker.addEventListener('click', function(ev, target){
+            if (animatedMarker.object3D.visible == true && ev.detail.cursorEl) {
+                const entity = document.querySelector('#animated-model');
+                const scale = entity.getAttribute('scale');
+                Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
+                entity.setAttribute('scale', scale);
+            }
+        });
+}});
